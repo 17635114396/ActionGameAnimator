cc52753e06d4dfb6cc479c7d2496807d3f36b98d
17635114396 <2769158060@qq.com>
2019-07-27 11:11:24 +0800

    Spawn And Note
    spawn脚本的协程思考及注释

--- Assets/C#Scripts/Spawn.cs
+++ Assets/C#Scripts/Spawn.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
+    //该脚本只写一个生成函数，但是不会运行，不负责控制，相当于提前规则，并声明。
     public GameObject prefab;
     // Start is called before the first frame update
     void Start()
@@ -16,8 +17,9 @@ public class Spawn : MonoBehaviour
     {
         
     }
-    public GameObject MySpawn() {
-
+    public GameObject MySpawn() {//生成预制体函数
         return GameObject.Instantiate(prefab, transform.position, transform.rotation) as GameObject; 
+        //用Instantiate方法生成，并返回该预制体，后期可以用来删除，因为有返回，所以函数类型为GameObject;
     }
+    //脚本写完后，挂载在每一个生成的位置点上。
 }
---
+++ Assets/C#Scripts/SpawnManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public Spawn[] monsterArry;//怪物数组【孵化位置】
+    public Spawn[] bossArry;//boss数组【孵化位置】
+
+    //list<T> boss,列表boss，类型为T
+    public List<GameObject> enemylist = new List<GameObject>();//敌人列表
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemySpawn());//开始协程
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator EnemySpawn() {//协程的具体内容，及其定义。
+        //第一波敌人的生成
+        foreach (Spawn s in monsterArry) {//实例化生成脚本对象，类型为monster.
+          enemylist.Add( s.MySpawn());//每孵化一个怪物，敌人列表添加一个元素，生成的该敌人。
+        }
+
+        //第一关过关条件
+        while (enemylist.Count > 0)//敌人列表是否为空，是否全部死亡，没死就再等着【yield】
+        {
+            //yield是协程的核心知识点，返回的值，即为开始执行的值。
+            yield return new WaitForSeconds(0.2f);//循环等待，退出值为count !>0;
+            
+        }
+
+
+        // 第二波敌人的生成
+        foreach (Spawn s in monsterArry)
+        {
+            enemylist.Add(s.MySpawn());//一组
+        }
+        yield return new WaitForSeconds(0.5f);//单纯等待一段时间
+        foreach (Spawn s in monsterArry)
+        {
+            enemylist.Add(s.MySpawn());//两组
+        }
+
+        //第二关过关条件
+        while (enemylist.Count > 0)
+        {
+            yield return new WaitForSeconds(0.2f);
+        }
+
+
+
+        //第三波敌人的生成
+        foreach (Spawn s in monsterArry)
+        {
+            enemylist.Add(s.MySpawn());//一组
+        }
+        while (enemylist.Count > 0)
+        {
+            yield return new WaitForSeconds(0.2f);
+        }
+        foreach (Spawn s in monsterArry)
+        {
+            enemylist.Add(s.MySpawn());//两组
+        }
+        yield return new WaitForSeconds(0.5f);
+        foreach (Spawn s in bossArry) {
+            enemylist.Add(s.MySpawn());//boss
+        }
+    }
+}
