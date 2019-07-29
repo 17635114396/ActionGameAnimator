af5b5c56103428d5d6c4a156593c7a601ce0e519
17635114396 <2769158060@qq.com>
2019-07-29 17:09:27 +0800

    Course26
    boss 的攻击完成

--- Assets/C#Scripts/SoulBossATKAndDamage.cs
+++ Assets/C#Scripts/SoulBossATKAndDamage.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoulMonsterATKAndDamage :ATKAndDamage
+public class SoulBossATKAndDamage :ATKAndDamage
 {
     private Transform player;
-
-     private new void Awake()
+    private new void Awake()
     {
         base.Awake();
         player = GameObject.FindGameObjectWithTag(Tags.player).transform;
     }
-    public void MonsterAttack() {
+    public void Attack01() {
+        if (Vector3.Distance(transform.position, player.position) < attackDistance)
+        {
+            player.GetComponent<ATKAndDamage>().TakeDamage(normalAttack);
+        }
+    }
+    public void Attack02() {
         if (Vector3.Distance(transform.position, player.position) < attackDistance)
         {
             player.GetComponent<ATKAndDamage>().TakeDamage(normalAttack);
